67e793b57f9dca2f8abf86dcfeab49fb496b108e
vishal29meghwal <81527294+vishal29meghwal@users.noreply.github.com>
2021-03-31 15:06:31 +0530

    Update 315A - Sereja and Bottles.cpp

--- 315A - Sereja and Bottles.cpp
+++ 315A - Sereja and Bottles.cpp
@@ -1,25 +1,24 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#define ll long long 
-using namespace std;
+#include <cstdio>
 
 int main()
 {
-    ll n , c = 0;
-    cin>>n;
-    ll a[n][2];
-    map<int,int> m;
-    for(ll i = 0 ; i < n ; i++){
-        cin>>a[i][0]>>a[i][1];
-        m[a[i][0]]=0;
+    int n, a[1000], b[1000], c[1001] = {0};
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d%d", &a[i], &b[i]);
+        c[b[i]] += 1;
     }
-    for(ll i=0 ; i<n ; i++){
-        if(a[i][0]!=a[i][1])m[a[i][1]]++;
+    int count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        c[b[i]] -= 1;
+        if (c[a[i]] == 0)
+        {
+            count += 1;
+        }
+        c[b[i]] += 1;
     }
-    for(ll i=0 ; i<n ; i++){
-        if(m[a[i][0]] == 0)c++;
-    }
-    cout<<c<<endl;
-    
+    printf("%d\n", count);
     return 0;
 }
